a2cd46eb72784e01e8c6fab8b923bbbaaaeeff93
Piyush Subedi <piyush.subedi@ambarkaar.com>
2018-08-25 22:46:00 +0545

    Simply committing because of battery drainage

--- Project 10/XMLWriter.java
+++ Project 10/XMLWriter.java
@@ -2,9 +2,29 @@
 
 public class XMLWriter() {
 
+    private final String EMPTY_STRING = "":
+
+
+    private String tempStore;
+
 
     public XMLWriter(String outputFileNam) {
+        this.tempStore = "";
+    }
+
+
+    public void addElement(String elemName, String elemValue) {
         
+    }
+
+    public void addElement(String elemName) {
+
+    }
+
+    private void start(String elemName) {
+
+        tempStore += ""
+
     }
 
 }
